b19b8abb2c1ea4e5a9b82459e8aabedcb98366ff
cristysandu <cristysandu3@gmail.com>
2020-10-21 12:33:34 +0300

    finish ex4 si ex5 lab 2

--- Laboratoare/Lab 2/test.sh
+++ Laboratoare/Lab 2/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-N=100
+N=1000
 P=2
 
 if [ ! -f "multiply_seq" ]
@@ -17,6 +17,14 @@ fi
 
 ./multiply_seq $N > seq.txt
 ./multiply $N $P > par.txt
+./multiply_4 $N $P > par_4.txt
+./multiply_5 $N $P > par_5.txt
+
+
 
 diff seq.txt par.txt 
+diff seq.txt par_4.txt 
+diff seq.txt par_5.txt 
+
+
 
